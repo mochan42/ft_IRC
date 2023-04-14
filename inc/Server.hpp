2c5bf1a24a819cae71f5f1caf95340e1a5b46e8b
pmeising <pmeising@student.42wolfsburg.de>
2023-04-14 21:11:24 +0200

    Added header

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,1 +1,31 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 21:10:11 by pmeising          #+#    #+#             */
+/*   Updated: 2023/04/14 21:11:12 by pmeising         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+# define SERVER_HPP
+# include <string>
+# include <iostream>
+
+class Server
+{
+	private:
+		
+	
+
+	public:
+		Server(/* args */);
+		~Server();
+};
+
+
+
+#endif
