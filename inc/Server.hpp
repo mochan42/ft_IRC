2a4c49b268490740a7bb1944453682cb690f166e
pmeising <pmeising@student.42wolfsburg.de>
2023-04-14 21:25:31 +0200

    Added private variables to Server.hpp

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 21:10:11 by pmeising          #+#    #+#             */
-/*   Updated: 2023/04/14 21:14:18 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/04/14 21:25:10 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,20 @@
 # include <string>
 # include <iostream>
 # include <map>
+# include "User.hpp"
+# include "Channel.hpp"
+
+class User;
+
+class Channel;
 
 class Server
 {
 	private:
+		std::map<int, User*>			_users;
+		std::map<std::string, Channel*>	_channels;
+		std::map<t_err, std::string>	_errors;
 		
-	
 
 	public:
 		Server(/* args */);
