94a26a2f1df2eeb44f42991ba60f73cf0bdcb95e
pmeising <pmeising@student.42wolfsburg.de>
2023-04-18 21:16:33 +0200

    Adjusted Makefile on test

--- test/src/test_server.cpp
+++ test/src/test_server.cpp
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 22:52:04 by pmeising          #+#    #+#             */
-/*   Updated: 2023/04/17 23:01:44 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/04/18 21:08:17 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ TEST_CASE( "Server : Server Constructor", "[Server]")
 	SECTION("Server Constructor NULL")
 	{
 		Server	server();
+		REQUIRE(server.);
+	}
+	SECTION("Server Constructor Port 50000, Password '' ")
+	{
+		Server	server(50000, "");
 		REQUIRE("Error: expected number of arguments is 2 : <server port number> <server password>");
 	}
-	SECTION("Server Constructor Port 50000, Password NULL")
+	SECTION("Server Constructor Port 50000, Password '' ")
 	{
-		Server	server(50000, "test");
-		REQUIRE("Error: expected number of arguments is 2 : <server port number> <server password>");
+		Server	server(50000, ".");
+		REQUIRE("Server Password is      : .");
 	}
 }
