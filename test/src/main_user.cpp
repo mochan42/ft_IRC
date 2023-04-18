e63701884137b6295d31a5a053bba43de4d80b9a
fsemke <fsemke@student.42wolfsburg.de>
2023-04-18 18:00:55 +0200

    added functions

--- test/src/main_user.cpp
+++ test/src/main_user.cpp
@@ -6,7 +6,7 @@
 /*   By: fsemke <fsemke@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 20:01:16 by cudoh             #+#    #+#             */
-/*   Updated: 2023/04/18 13:43:43 by fsemke           ###   ########.fr       */
+/*   Updated: 2023/04/18 16:32:30 by fsemke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@
 	REQUIRE(user.getNickName() == "florian_nick");
 } */
 
-TEST_CASE("Test setUsername, getUsername") {
+/* TEST_CASE("Test setUsername, getUsername") {
 	User user(4, 0);
 	std::vector<std::string> args;
     args.push_back("UserName");
 
 	user.setUserName(args);
 	REQUIRE(user.getUserName() == "UserName");
-}
+} */
 
 /* TEST_CASE("Test setRealName, getRealName") {
 	User user(4, 0);
@@ -45,3 +45,16 @@ TEST_CASE("Test setUsername, getUsername") {
 	user.setRealName("Crazy Name");
 	REQUIRE(user.getRealName() == "Crazy Name");
 } */
+
+TEST_CASE("Test combine_args") {
+	std::vector<std::string> args;
+	args.push_back(":Hello");
+	args.push_back("i'm");
+	args.push_back("a");
+	args.push_back(":list");
+
+	User user(4, 255, NULL);
+	
+	REQUIRE(user.combine_args(args.begin(), args.end()) == (std::string)"Hello i'm a :list");
+	REQUIRE(user.combine_args(args.begin() + 3, args.end()) == (std::string)"list");
+}
