99d1921376597751546c92f9d580d8d677c96736
pmeising <pmeising@student.42wolfsburg.de>
2023-04-26 11:16:27 +0200

    .

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsemke <fsemke@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 21:10:11 by pmeising          #+#    #+#             */
-/*   Updated: 2023/04/25 16:16:01 by fsemke           ###   ########.fr       */
+/*   Updated: 2023/04/26 11:16:05 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,8 +71,7 @@ class Server
 		~Server();
 		// void			startServer(void);
 		// void			closeServer(void);
-		
-		Channel*				createChannel(const std::string& channel) const;
+
 		void					pingClient(int client_socket);
 		Channel*				createChannel(const std::string& channel_name, const std::string& topic, User* user);
 		void					pingClient(void) const;
@@ -90,11 +89,9 @@ class Server
 		// setters
 		void					setListeningSocket (int n);
 		void					setPort(int inputPortNumber);
-	//	void					checkPassword(const std::string& password) const;
 		void					setServerIP(std::string setServerIP);
 
 		// member functions
-		void					checkPassword(const std::string& password) const;
 		void					createSocket();
 		void					makeListeningSocketReusable();
 		void					setSocketToNonBlocking();
